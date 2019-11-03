083507e1f8ccec997821235a752bba153034e666
Mateo Tutic <mateo@tutic.ch>
2019-11-03 17:27:58 +0100

    We know print 10 movies in the pretty bootstrap template

--- src/main/java/ch/heigvd/amt/presentation/HomeServlet.java
+++ src/main/java/ch/heigvd/amt/presentation/HomeServlet.java
@@ -1,4 +1,24 @@
 package ch.heigvd.amt.presentation;
 
-public class HomeServlet {
+import ch.heigvd.amt.integration.IMoviesDAO;
+
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "HomeServlet", urlPatterns = "")
+public class HomeServlet extends HttpServlet {
+    @EJB
+    private IMoviesDAO moviesManager;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=UTF-8");
+        req.setAttribute("movies", moviesManager.findAllMovies());
+        req.getRequestDispatcher("index.jsp").forward(req, resp);
+    }
 }
