c1d3b3d62856e7df8181fc72bf692730eb7cb110
Mateo Tutic <mateo@tutic.ch>
2019-11-05 23:46:40 +0100

    We have to be authenticated now

--- src/main/java/ch/heigvd/amt/presentation/SecurityFilter.java
+++ src/main/java/ch/heigvd/amt/presentation/SecurityFilter.java
@@ -2,17 +2,37 @@ package ch.heigvd.amt.presentation;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 @WebFilter("/*")
 public class SecurityFilter implements Filter {
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-    }
+    // Paths to be ignored by the filter
+    private static final Set ALLOWED_PATHS = Collections.unmodifiableSet(new HashSet<>(Arrays.asList("/login",
+            "/logout", "/register")));
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        filterChain.doFilter(servletRequest, servletResponse);
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        String path = request.getRequestURI().substring(request.getContextPath().length()).replaceAll("[/]+$", "");
+
+        // Check if the user id is stored in session
+        if (request.getSession().getAttribute("user") != null || ALLOWED_PATHS.contains(path)) {
+            filterChain.doFilter(servletRequest, servletResponse);
+        } else {
+            // The user is not connected, redirect to the login page
+            response.sendRedirect(request.getContextPath() + "/login");
+        }
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
     }
 
     @Override
