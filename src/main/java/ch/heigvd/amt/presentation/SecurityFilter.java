4c0e47c2081583350c8ab094c0a4ebc5acf5949b
Mateo Tutic <mateo@tutic.ch>
2019-11-06 09:44:39 +0100

    remove unused imports

--- src/main/java/ch/heigvd/amt/presentation/SecurityFilter.java
+++ src/main/java/ch/heigvd/amt/presentation/SecurityFilter.java
@@ -5,10 +5,6 @@ import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
 
 @WebFilter(urlPatterns = {"", "/logout"})
 public class SecurityFilter implements Filter {
