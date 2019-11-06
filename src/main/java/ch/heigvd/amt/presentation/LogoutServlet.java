13ae1b5d6f085163c4a1f9ec36e43b5f95505d07
Mateo Tutic <mateo@tutic.ch>
2019-11-06 10:27:52 +0100

    Logout implemented

---
+++ src/main/java/ch/heigvd/amt/presentation/LogoutServlet.java
@@ -0,0 +1,20 @@
+package ch.heigvd.amt.presentation;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "LogoutServlet", urlPatterns = "/logout")
+public class LogoutServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // Unbound any objects bound to the session
+        req.getSession().invalidate();
+
+        // Redirect to the login page
+        req.getRequestDispatcher("/WEB-INF/pages/login.jsp").forward(req, resp);
+    }
+}
