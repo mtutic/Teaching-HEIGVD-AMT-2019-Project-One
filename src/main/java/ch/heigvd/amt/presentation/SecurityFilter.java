d7355dedcab19b566d4d5362a6ed643b33856f4c
Mateo Tutic <mateo@tutic.ch>
2019-11-06 09:42:45 +0100

    If I don't do like that, css files and other resources won't be loaded correctly

--- src/main/java/ch/heigvd/amt/presentation/SecurityFilter.java
+++ src/main/java/ch/heigvd/amt/presentation/SecurityFilter.java
@@ -10,20 +10,15 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
-@WebFilter("/*")
+@WebFilter(urlPatterns = {"", "/logout"})
 public class SecurityFilter implements Filter {
-    // Paths to be ignored by the filter
-    private static final Set ALLOWED_PATHS = Collections.unmodifiableSet(new HashSet<>(Arrays.asList("/login",
-            "/logout", "/register")));
-
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
-        String path = request.getRequestURI().substring(request.getContextPath().length()).replaceAll("[/]+$", "");
 
         // Check if the user id is stored in session
-        if (request.getSession().getAttribute("user") != null || ALLOWED_PATHS.contains(path)) {
+        if (request.getSession().getAttribute("user") != null) {
             filterChain.doFilter(servletRequest, servletResponse);
         } else {
             // The user is not connected, redirect to the login page
